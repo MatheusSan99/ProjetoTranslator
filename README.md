18a962ec20404dd298c5f90919f5bb63d292b0f8
Matheus Santos <matheus.oliveira@ultralims.com.br>
2023-11-05 14:21:25 -0300

    commit inicial com o esqueleto do projeto

--- README.md
+++
@@ -1,2 +0,0 @@
-# ProjetoTranslator
-Este projeto pessoal tem como objetivo o desenvolvimento de uma aplicação desktop em PHP. O foco principal está na análise das regras de negócio propostas, bem como na revisão e tradução dos documentos enviados.
---
+++ ui.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use MatheusSan\Translator\Ui\MainWindow;
+use Tkui\Environment;
+use Tkui\TclTk\TkAppFactory;
+
+require_once 'vendor/autoload.php';
+
+
+$environment = new class implements Environment
+{
+    private static array $environmentData = [
+        'WINDOWS_LIB_TCL' => '.\IronTcl\bin\tcl86t.dll',
+        'WINDOWS_LIB_TK' => '.\IronTcl\bin\tk86t.dll',
+        'DEBUG' => true,
+    ];
+
+    public function getValue(string $param, $default = null)
+    {
+        return self::$environmentData[$param] ?? $default;
+    }
+};
+$factory = new TkAppFactory('Tradutor Automatico');
+$app = $factory->createFromEnvironment($environment);
+
+// TODO: Refactor this class because all the code lives there so far
+$mainWindow = new MainWindow($app, 'Tradutor Automatico');
+$mainWindow->draw();
+
+$app->run();
